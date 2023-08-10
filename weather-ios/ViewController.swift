155959d6b607ca9a17bfb7d6db4f6dfcada0c0cb
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-08-10 20:12:22 -0300

    starting first view

--- weather-ios/ViewController.swift
+++ weather-ios/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    
+    @IBOutlet weak var conditionLabel: UILabel!
+  
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
