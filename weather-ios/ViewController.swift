d73d7bae2242de011c0915227e766771ceb1871b
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-08-11 16:10:20 -0300

    feature: adjusting

--- weather-ios/ViewController.swift
+++ weather-ios/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  weather-ios
-//
-//  Created by Eduardo Wanderley de Siqueira Araújo on 09/08/23.
-//
-
 import UIKit
 import Alamofire
 import SwiftyJSON
@@ -12,40 +5,46 @@ import NVActivityIndicatorView
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    
+
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dayLabel: UILabel!
-    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var conditionLabel: UILabel!
-  
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var backgroundView: UIView!
-    
+
     let gradientLayer = CAGradientLayer()
-    
+
     let apiKey = "7377a133b9425bff224fc293a04f46af"
     var lat = 11.344533
     var lon = 104.33322
-    var activityIndicator: NVActivityIndicatorView
+    var activityIndicator: NVActivityIndicatorView! // Declare it as an optional
     let locationManager = CLLocationManager()
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Initialize the gradient background
         backgroundView.layer.addSublayer(gradientLayer)
         
+        // Calculate the indicator frame
         let indicatorSize: CGFloat = 70
-        let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
-        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color:UIColor.white, padding: 20.0)
+        let indicatorFrame = CGRect(x: (view.frame.width - indicatorSize) / 2,
+                                    y: (view.frame.height - indicatorSize) / 2,
+                                    width: indicatorSize, height: indicatorSize)
+        
+        // Initialize activityIndicator here
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
         activityIndicator.backgroundColor = UIColor.black
         view.addSubview(activityIndicator)
         
+        // Request location services authorization
         locationManager.requestWhenInUseAuthorization()
         
+        // Start animating the activity indicator
         activityIndicator.startAnimating()
-        if(CLLocationManager.locationServicesEnabled()){
+        
+        if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
@@ -56,44 +55,39 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         setBlueGradientBackground()
     }
     
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
         let location = locations[0]
         lat = location.coordinate.latitude
         lon = location.coordinate.longitude
-        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON{
-            response in
+        
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON { response in
             self.activityIndicator.stopAnimating()
+            
             if let responseStr = response.result.value {
                 let jsonResponse = JSON(responseStr)
                 let jsonWeather = jsonResponse["weather"].array![0]
                 let jsonTemp = jsonResponse["main"]
-                let iconName = jsonWeather ["icon"].stringValue
-                
+                let iconName = jsonWeather["icon"].stringValue
+
                 self.locationLabel.text = jsonResponse["name"].stringValue
                 self.conditionImageView.image = UIImage(named: iconName)
                 self.conditionLabel.text = jsonWeather["main"].stringValue
+                self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))°C"
             }
         }
-        
     }
-    
-    
-    
-    func setBlueGradientBackground(){
+
+    func setBlueGradientBackground() {
         let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
         let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [topColor, bottomColor]
     }
-    
-    func setGreyGradientBackground(){
+
+    func setGreyGradientBackground() {
         let topColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0).cgColor
         let bottomColor = UIColor(red: 72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha: 1.0).cgColor
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [topColor, bottomColor]
     }
-
 }
-
